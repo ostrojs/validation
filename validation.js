c1590e7b6319a7764a75fb37ade711560cc89e36
Amar kumar singh <amar@ostrojs.com>
2023-12-27 19:17:21 +0530

    Develop (#7)
    * file validation bug fix
    
    * bug fix for mime validation
    
    * bug fix for mimes validation
    
    * bug fix for mimes

--- validation.js
+++ validation.js
@@ -4,7 +4,7 @@ var Lang = require('./lang');
 var Attributes = require('./attributes');
 var AsyncResolvers = require('./async');
 var validationException = require('./validationException');
-var Validator = function(input, rules, customMessages) {
+var Validator = function (input, rules, customMessages) {
     var lang = Validator.getDefaultLang();
     this.input = input || {};
 
@@ -29,10 +29,10 @@ Validator.prototype = {
 
     attributeFormatter: Attributes.formatter,
 
-    errors: function() {
+    errors: function () {
         return this.errorBag
     },
-    check: function() {
+    check: async function () {
         var self = this;
 
         for (var attribute in this.rules) {
@@ -51,7 +51,7 @@ Validator.prototype = {
                     continue;
                 }
 
-                rulePassed = rule.validate(inputValue, ruleOptions.value, attribute);
+                rulePassed = await rule.validate(inputValue, ruleOptions.value, attribute);
                 if (!rulePassed) {
                     this._addFailure(rule);
                 }
@@ -65,16 +65,16 @@ Validator.prototype = {
         return this.errorCount === 0;
     },
 
-    checkAsync: function(passes, fails) {
+    checkAsync: function (passes, fails) {
         var _this = this;
-        passes = passes || function() {};
-        fails = fails || function() {};
+        passes = passes || function () { };
+        fails = fails || function () { };
 
-        var failsOne = function(rule, message) {
+        var failsOne = function (rule, message) {
             _this._addFailure(rule, message);
         };
 
-        var resolvedAll = function(allPassed) {
+        var resolvedAll = function (allPassed) {
             if (allPassed) {
                 passes();
             } else {
@@ -84,10 +84,10 @@ Validator.prototype = {
 
         var asyncResolvers = new AsyncResolvers(failsOne, resolvedAll);
 
-        var validateRule = function(inputValue, ruleOptions, attribute, rule) {
-            return function() {
+        var validateRule = function (inputValue, ruleOptions, attribute, rule) {
+            return function () {
                 var resolverIndex = asyncResolvers.add(rule);
-                rule.validate(inputValue, ruleOptions.value, attribute, function() {
+                rule.validate(inputValue, ruleOptions.value, attribute, function () {
                     asyncResolvers.resolve(resolverIndex);
                 });
             };
@@ -118,13 +118,13 @@ Validator.prototype = {
         asyncResolvers.fire();
     },
 
-    _addFailure: function(rule) {
+    _addFailure: function (rule) {
         var msg = this.messages.render(rule);
         this.errorBag.add(rule.attribute, msg);
         this.errorCount++;
     },
 
-    _flattenObject: function(obj) {
+    _flattenObject: function (obj) {
         var flattened = {};
 
         function recurse(current, property) {
@@ -150,7 +150,7 @@ Validator.prototype = {
         return flattened;
     },
 
-    _objectPath: function(obj, path) {
+    _objectPath: function (obj, path) {
         if (Object.prototype.hasOwnProperty.call(obj, path)) {
             return obj[path];
         }
@@ -173,7 +173,7 @@ Validator.prototype = {
         return copy;
     },
 
-    _parseRules: function(rules) {
+    _parseRules: function (rules) {
 
         var parsedRules = {};
         rules = this._flattenObject(rules);
@@ -188,7 +188,7 @@ Validator.prototype = {
 
     },
 
-    _parseRulesCheck: function(attribute, rulesArray, parsedRules, wildCardValues) {
+    _parseRulesCheck: function (attribute, rulesArray, parsedRules, wildCardValues) {
         if (attribute.indexOf('*') > -1) {
             this._parsedRulesRecurse(attribute, rulesArray, parsedRules, wildCardValues);
         } else {
@@ -196,7 +196,7 @@ Validator.prototype = {
         }
     },
 
-    _parsedRulesRecurse: function(attribute, rulesArray, parsedRules, wildCardValues) {
+    _parsedRulesRecurse: function (attribute, rulesArray, parsedRules, wildCardValues) {
         var parentPath = attribute.substr(0, attribute.indexOf('*') - 1);
         var propertyValue = this._objectPath(this.input, parentPath);
 
@@ -209,7 +209,7 @@ Validator.prototype = {
         }
     },
 
-    _parseRulesDefault: function(attribute, rulesArray, parsedRules, wildCardValues) {
+    _parseRulesDefault: function (attribute, rulesArray, parsedRules, wildCardValues) {
         var attributeRules = [];
 
         if (rulesArray instanceof Array) {
@@ -236,14 +236,14 @@ Validator.prototype = {
         parsedRules[attribute] = attributeRules;
     },
 
-    _replaceWildCards: function(path, nums) {
+    _replaceWildCards: function (path, nums) {
 
         if (!nums) {
             return path;
         }
 
         var path2 = path;
-        nums.forEach(function(value) {
+        nums.forEach(function (value) {
             if (Array.isArray(path2)) {
                 path2 = path2[0];
             }
@@ -260,10 +260,10 @@ Validator.prototype = {
         return path2;
     },
 
-    _replaceWildCardsMessages: function(nums) {
+    _replaceWildCardsMessages: function (nums) {
         var customMessages = this.messages.customMessages;
         var self = this;
-        Object.keys(customMessages).forEach(function(key) {
+        Object.keys(customMessages).forEach(function (key) {
             if (nums) {
                 var newKey = self._replaceWildCards(key, nums);
                 customMessages[newKey] = customMessages[key];
@@ -273,7 +273,7 @@ Validator.prototype = {
         this.messages._setCustom(customMessages);
     },
 
-    _prepareRulesArray: function(rulesArray) {
+    _prepareRulesArray: function (rulesArray) {
         var rules = [];
 
         for (var i = 0, len = rulesArray.length; i < len; i++) {
@@ -292,11 +292,11 @@ Validator.prototype = {
         return rules;
     },
 
-    _suppliedWithData: function(attribute) {
+    _suppliedWithData: function (attribute) {
         return this.input.hasOwnProperty(attribute);
     },
 
-    _extractRuleAndRuleValue: function(ruleString) {
+    _extractRuleAndRuleValue: function (ruleString) {
         var rule = {},
             ruleArray;
 
@@ -311,7 +311,7 @@ Validator.prototype = {
         return rule;
     },
 
-    _hasRule: function(attribute, findRules) {
+    _hasRule: function (attribute, findRules) {
         var rules = this.rules[attribute] || [];
         for (var i = 0, len = rules.length; i < len; i++) {
             if (findRules.indexOf(rules[i].name) > -1) {
@@ -321,11 +321,11 @@ Validator.prototype = {
         return false;
     },
 
-    _hasNumericRule: function(attribute) {
+    _hasNumericRule: function (attribute) {
         return this._hasRule(attribute, this.numericRules);
     },
 
-    _isValidatable: function(rule, value) {
+    _isValidatable: function (rule, value) {
         if (Rules.isImplicit(rule.name)) {
             return true;
         }
@@ -333,7 +333,7 @@ Validator.prototype = {
         return this.getRule('required').validate(value);
     },
 
-    _shouldStopValidating: function(attribute, rulePassed) {
+    _shouldStopValidating: function (attribute, rulePassed) {
 
         var stopOnAttributes = this.stopOnAttributes;
         if (typeof stopOnAttributes === 'undefined' || stopOnAttributes === false || rulePassed === true) {
@@ -347,23 +347,23 @@ Validator.prototype = {
         return true;
     },
 
-    setAttributeNames: function(attributes) {
+    setAttributeNames: function (attributes) {
         this.messages._setAttributeNames(attributes);
     },
 
-    setAttributeFormatter: function(func) {
+    setAttributeFormatter: function (func) {
         this.messages._setAttributeFormatter(func);
     },
 
-    getRule: function(name) {
+    getRule: function (name) {
         return Rules.make(name, this);
     },
 
-    stopOnError: function(attributes) {
+    stopOnError: function (attributes) {
         this.stopOnAttributes = attributes;
     },
 
-    passes: function(passes) {
+    passes: function (passes) {
         var async = this._checkAsync('passes', passes);
         if (async) {
             return this.checkAsync(passes);
@@ -371,15 +371,15 @@ Validator.prototype = {
         return this.check();
     },
 
-    fails: function(fails) {
+    fails: async function (fails) {
         var async = this._checkAsync('fails', fails);
         if (async) {
-            return this.checkAsync(function() {}, fails);
+            return this.checkAsync(function () { }, fails);
         }
-        return !this.check();
+        return !await this.check();
     },
 
-    _checkAsync: function(funcName, callback) {
+    _checkAsync: function (funcName, callback) {
         var hasCallback = typeof callback === 'function';
         if (this.hasAsync && !hasCallback) {
             throw funcName + ' expects a callback when async rules are being tested.';
@@ -390,56 +390,56 @@ Validator.prototype = {
 
 };
 
-Validator.setMessages = function(lang, messages) {
+Validator.setMessages = function (lang, messages) {
     Lang._set(lang, messages);
     return this;
 };
 
-Validator.getMessages = function(lang) {
+Validator.getMessages = function (lang) {
     return Lang._get(lang);
 };
 
-Validator.useLang = function(lang) {
+Validator.useLang = function (lang) {
     this.prototype.lang = lang;
 };
 
-Validator.getDefaultLang = function() {
+Validator.getDefaultLang = function () {
     return this.prototype.lang;
 };
 
-Validator.setAttributeFormatter = function(func) {
+Validator.setAttributeFormatter = function (func) {
     this.prototype.attributeFormatter = func;
 };
 
-Validator.stopOnError = function(attributes) {
+Validator.stopOnError = function (attributes) {
     this.prototype.stopOnAttributes = attributes;
 };
 
-Validator.register = function(name, fn, message) {
+Validator.register = function (name, fn, message) {
     var lang = Validator.getDefaultLang();
     Rules.register(name, fn);
     Lang._setRuleMessage(lang, name, message);
 };
 
-Validator.registerImplicit = function(name, fn, message) {
+Validator.registerImplicit = function (name, fn, message) {
     var lang = Validator.getDefaultLang();
     Rules.registerImplicit(name, fn);
     Lang._setRuleMessage(lang, name, message);
 };
 
-Validator.registerAsync = function(name, fn, message) {
+Validator.registerAsync = function (name, fn, message) {
     var lang = Validator.getDefaultLang();
     Rules.registerAsync(name, fn);
     Lang._setRuleMessage(lang, name, message);
 };
 
-Validator.registerAsyncImplicit = function(name, fn, message) {
+Validator.registerAsyncImplicit = function (name, fn, message) {
     var lang = Validator.getDefaultLang();
     Rules.registerAsyncImplicit(name, fn);
     Lang._setRuleMessage(lang, name, message);
 };
 
-Validator.registerMissedRuleValidator = function(fn, message) {
+Validator.registerMissedRuleValidator = function (fn, message) {
     Rules.registerMissedRuleValidator(fn, message);
 };
 
